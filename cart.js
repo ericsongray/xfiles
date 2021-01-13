9c9ff37b185c245db5d3ae0f6b5ff5f193ea6f91
XavierVanAlstyne <xvvanalstyne@gmail.com>
2021-01-13 18:56:10 -0500

    finished project

--- cart.js
+++ cart.js
@@ -1,11 +1,22 @@
 const fruits = require('./fruits');
 
-/* refactor this cart into a function with prototype methods  */
+/* refactor this cart into a function with prototype methods  
 let cart = [];
 const addItem = function (item) {
   cart.push(item)
 };
-
+*/
 /* Prototype Cart */
 
+function Cart(){
+  this.items = []
+};
 
+Cart.prototype.add = function(item){
+  this.items.push(item)
+  console.log(`${item.name} added to cart`)
+};
+Cart.prototype.remove = function(item){
+  this.items.pop(item)
+  console.log(`${item.name} removed from cart`)
+}
