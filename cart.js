162914863a44276ebb943f792c4c62029a6b808a
Eric Gray <ericlamargray@gmail.com>
2021-01-13 20:59:51 -0500

    start over

--- cart.js
+++ cart.js
@@ -8,15 +8,3 @@ const addItem = function (item) {
 */
 /* Prototype Cart */
 
-function Cart(){
-  this.items = []
-};
-
-Cart.prototype.add = function(item){
-  this.items.push(item)
-  console.log(`${item.name} added to cart`)
-};
-Cart.prototype.remove = function(item){
-  this.items.pop(item)
-  console.log(`${item.name} removed from cart`)
-}
